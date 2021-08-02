1652c359b57b2c2c1b9e052d40378409baab317e
Sobrino Julian <sobrinojulian@protonmail.com>
2021-08-02 14:49:56 -0300

    Imprime tabla completa

--- module.js
+++ module.js
@@ -1,9 +1,32 @@
+#!/usr/bin/env node
+
+import https from "https";
+import Table from "cli-table3";
+
 const nueveZeta = (csgo) => {
-	const endpoint = 'https://9z.games/api/partidos'
-	if (csgo) {
-		return `CS:GO Hello World!`;
-	}
-	return `Hello World!`;
+  const url = "https://9z.games/api/partidos";
+  https.get(url, (res) => {
+    let data = "";
+    res.on("data", (chunk) => (data += chunk));
+    res.on("end", () => {
+      const partidos = JSON.parse(data);
+      let table = new Table();
+      for (const partido of partidos) {
+        const esAcademy = partido.equipo.nombre === "Academy";
+        const esCSGO = partido.torneo.juego.nombre === "CS:GO";
+        table.push([
+          partido.fecha,
+          partido.hora,
+          partido.vs,
+          partido.torneo.nombre,
+          partido.torneo.juego.nombre,
+          esAcademy ? "Academy" : "",
+          esCSGO,
+        ]);
+      }
+	  console.log(table.toString());
+    });
+  });
 };
 
 export default nueveZeta;
